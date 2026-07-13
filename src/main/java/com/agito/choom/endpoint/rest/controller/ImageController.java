b2b5bede9b1f2cf73819eacd5c07d868736e38e6
Diarilala <hei.diarilala@gmail.com>
2026-07-13 10:12:23 +0300

    feat: add controller

--- src/main/java/com/agito/choom/endpoint/rest/controller/ImageController.java
+++ src/main/java/com/agito/choom/endpoint/rest/controller/ImageController.java
@@ -7,10 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -30,5 +27,10 @@ public class ImageController {
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
+  }
+
+  @GetMapping
+  public ResponseEntity<?> findAll() {
+    return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
   }
 }
