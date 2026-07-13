87ba16a70157ba15259ae0e3d8540234ff035723
Diarilala <hei.diarilala@gmail.com>
2026-07-13 08:49:33 +0300

    feat: add event

---
+++ src/main/java/com/agito/choom/endpoint/event/model/ImageUploaded.java
@@ -0,0 +1,27 @@
+package com.agito.choom.endpoint.event.model;
+
+import com.agito.choom.model.Image;
+import lombok.*;
+
+import java.time.Duration;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+
+public class ImageUploaded extends PojaEvent{
+    private Image image;
+
+    @Override
+    public Duration maxConsumerDuration() {
+        return Duration.ofSeconds(45);
+    }
+
+    @Override
+    public Duration maxConsumerBackoffBetweenRetries() {
+        return Duration.ofSeconds(60);
+    }
+}
